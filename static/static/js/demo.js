b3ae57abe9f95311fcddc9056dcf1919f5b1b711
EKR <ekr@rtfm.com>
2011-10-20 12:27:09 -0700

    Remove XHR

--- static/static/js/demo.js
+++ static/static/js/demo.js
@@ -7,6 +7,40 @@ var ui_log = function(msg) {
 };
 
 
+// simple wrapper around XHR. The cool kids use JQuery here, but
+// we want a self-contained example
+var ajax = function(params) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(
+	params.type || "GET",
+	params.url,
+	true);
+    if(params.contentType){
+	xhr.setRequestHeader("Content-Type",params.contentType)
+    }
+    if(params.data){
+	xhr.send(params.data);
+    }
+    else{
+	xhr.send();
+    }
+    xhr.onreadystatechange = function() {
+	if(xhr.readyState === 4) {
+	    if(xhr.status === 200) {
+		if(params.success){
+		    params.success(xhr.responseText);
+		}
+	    }
+	    else {
+		if (params.error) {
+		    params.error(xhr.responseText);
+		}
+	    }
+	}
+    }
+};
+
+
 var ROAPClient = function(username, peer, start_call) {
     var poll_timeout = 1000; // ms
     
@@ -20,12 +54,12 @@ var ROAPClient = function(username, peer, start_call) {
 
 	log("Sending: " + JSON.stringify(msg));
 
-	$.ajax({
-		   url : "/msg/",
-		   type:"POST",
-		   contentType:"application/json",
-		   data: JSON.stringify(msg),
-	       });
+	ajax({
+		 url : "/msg/",
+		 type:"POST",
+		 contentType:"application/json",
+		 data: JSON.stringify(msg),
+	     });
     };
 
     var poll_success = function(msg) {
@@ -40,11 +74,11 @@ var ROAPClient = function(username, peer, start_call) {
     };
 
     var poll = function() {
-	$.ajax({
-		   url: "/msg/" + username + "/",
-		   success:poll_success,
-		   error:poll_error
-	       });
+	ajax({
+		 url: "/msg/" + username + "/",
+		 success:poll_success,
+		 error:poll_error
+	     });
     };
 
     var pc = new PeerConnection({}, signaling);
